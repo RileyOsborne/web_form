8f20157aa47f1006257094a7a1ee63b3daa4a343
Riley <riley.osborne@webbedsphere.com>
2021-11-16 09:12:16 -0500

    converted isset ternaries to null coalescing operators

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
  * with the data after a form submit
  */
 
-$chargeType = isset($_POST['charge-type']) ? $_POST['charge-type'] : '';
-$chargeTitle = isset($_POST['charge-title']) ? $_POST['charge-title'] : '';
-$chargeDescription = isset($_POST['charge-description']) ? $_POST['charge-description'] : '';
-$chargeAmount = isset($_POST['charge-amount']) ? $_POST['charge-amount'] : 0;
-$chargeCategory = isset($_POST['charge-category']) ? $_POST['charge-category'] : 1;
-$activeDate = isset($_POST['active-date']) ? $_POST['active-date'] : '';
-$dueDate = isset($_POST['due-date']) ? $_POST['due-date'] : '';
-$earlyPayment = isset($_POST['early-payment']) ? $_POST['early-payment'] : '';
-$paidInFullAmount = isset($_POST['paid-in-full-amount']) ? $_POST['paid-in-full-amount'] : 0.00;
-$paidInFullDays = isset($_POST['paid-in-full-days']) ? $_POST['paid-in-full-days'] : 0;
-$lateFee = isset($_POST['late-fee']) ? $_POST['late-fee'] : '';
-$paidAfterAmount = isset($_POST['paid-after-amount']) ? $_POST['paid-after-amount'] : 0.00;
-$paidAfterDays = isset($_POST['paid-after-days']) ? $_POST['paid-after-days'] : 0;
+$chargeType = $_POST['charge-type'] ?? '';
+$chargeTitle = $_POST['charge-title'] ?? '';
+$chargeDescription = $_POST['charge-description'] ?? '';
+$chargeAmount = $_POST['charge-amount'] ?? 0;
+$chargeCategory = $_POST['charge-category'] ?? 1;
+$activeDate = $_POST['active-date'] ?? '';
+$dueDate = $_POST['due-date'] ?? '';
+$earlyPayment = $_POST['early-payment'] ?? '';
+$paidInFullAmount = $_POST['paid-in-full-amount'] ?? 0.00;
+$paidInFullDays = $_POST['paid-in-full-days'] ?? 0;
+$lateFee = $_POST['late-fee'] ?? '';
+$paidAfterAmount = $_POST['paid-after-amount'] ?? 0.00;
+$paidAfterDays = $_POST['paid-after-days'] ?? 0;
 
 
 require 'index/index.view.php';
